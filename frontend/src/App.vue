941075ebfac794a38141433374d27623050c01bf
Harcyldo Winkelmann <harcyldopax@gmail.com>
2025-11-19 20:48:02 -0400

    feat: Adicionando a exibição dos lançamentos

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -11,17 +11,17 @@
           
             <!-- Coluna de 'Receitas' -->
             <v-col cols="12" md="4">
-                <Body title="Receitas" @request-add="openModal" />
+                <Body title="Receitas" :items="receitas" @request-add="openModal" />
             </v-col>
 
             <!-- COLUNA 2: DESPESAS FIXAS -->
             <v-col cols="12" md="4">
-                <Body title="Despesas Fixas" @request-add="openModal" />
+                <Body title="Despesas Fixas" :items="despesasFixas" @request-add="openModal" />
             </v-col>
 
             <!-- COLUNA 3: DESPESAS VARIÁVEIS -->
             <v-col cols="12" md="4">
-                <Body title="Despesas Variáveis" @request-add="openModal" />
+                <Body title="Despesas Variáveis" :items="despesasVariaveis" @request-add="openModal" />
             </v-col>
           </v-row>
       </v-container>
@@ -39,15 +39,38 @@
 import Metricas from './components/Metricas.vue'
 import Body from './components/Body.vue';
 import CadastroModal from './components/CadastroModal.vue';
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
+import { BuscarLancamentos } from '../wailsjs/go/main/App';
 
 const isModalOpen = ref(false);
 const selectedCategory = ref('');
+
+const receitas = ref([]);
+const despesasFixas = ref([]);
+const despesasVariaveis = ref([]);
 
 function openModal(categoryTitle) {
   selectedCategory.value = categoryTitle;
   isModalOpen.value = true;
 }
+
+async function loadAllData() {
+  try {
+    receitas.value = await BuscarLancamentos({ categoria: "Receitas" });
+
+    despesasFixas.value = await BuscarLancamentos({ categoria: "Despesas Fixas" });
+
+    despesasVariaveis.value = await BuscarLancamentos({ categoria: "Despesas Variáveis" });
+    
+    console.log("Dados recarregados do SQLite com sucesso.");
+  } catch (err) {
+    console.error("Erro ao carregar dados:", err);
+  }
+}
+
+onMounted(() => {
+  loadAllData();
+});
 
 // Chama quando o modal termina de salvar com sucesso
 function onTransactionSaved() {
@@ -56,15 +79,15 @@ function onTransactionSaved() {
 </script>
 
 <style>
-#logo {
-  display: block;
-  width: 50%;
-  height: 50%;
-  margin: auto;
-  padding: 10% 0 0;
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: 100% 100%;
-  background-origin: content-box;
-}
+  #logo {
+    display: block;
+    width: 50%;
+    height: 50%;
+    margin: auto;
+    padding: 10% 0 0;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
+    background-origin: content-box;
+  }
 </style>
--- frontend/src/components/Body.vue
+++ frontend/src/components/Body.vue
@@ -13,22 +13,44 @@
 
     <v-divider></v-divider>
 
-    <v-card-text>
-      <div v-if="items.length === 0" class="text-caption text-disabled">
+    <v-card-text class="pa-0 overflow-y-auto" style="max-height: 400px;">
+
+      <div v-if="items.length === 0" class="pa-4 text-caption text-disabled text-center">
         Nenhum lançamento.
       </div>
+
+      <v-list v-else lines="two" density="compact">
+        <v-list-item
+          v-for="item in items"
+          :key="item.id"
+          :title="item.descricao"
+          :subtitle="formatData(item.data)"
+        >
+          <template v-slot:append>
+            <div
+              class="text-body-2 font-weight-bold"
+            >
+              {{  formatCurrency(item.valor)  }}
+            </div>
+          </template>
+        </v-list-item>
+      </v-list>
+
     </v-card-text>
 
     <v-divider></v-divider>
-    <v-card-actions class="pa-4">
+
+    <v-card-actions class="pa-4 bg-surface-light">
       <strong>Total:</strong>
       <v-spacer></v-spacer>
-      <strong>R$ 0,00</strong>
+      <strong>{{ formatCurrency(totalValue) }}</strong>
     </v-card-actions>
   </v-card>
 </template>
 
 <script setup>
+import { computed } from 'vue';
+
 const props = defineProps({
   title: {
     type: String,
@@ -44,5 +66,22 @@ const emit = defineEmits(['request-add']);
 
 function onAddClick() {
   emit('request-add', props.title)
+}
+
+const totalValue = computed(() => {
+  return props.items.reduce((acc, item) => acc + item.valor, 0)
+});
+
+function formatCurrency(value) {
+  return new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(value);
+}
+
+function formatData(isoDate) {
+  if (!isoDate) return '';
+  const [year, month, day] = isoDate.split('-');
+  return `${day}/${month}/${year}`;
 }
 </script>
